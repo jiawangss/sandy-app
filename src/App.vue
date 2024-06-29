eea3afc15f508faf2a13016f30f2884ccae8982d
Sandy Wang <sandy@SandydeMacBook-Air.local>
2024-06-29 15:46:05 +0800

    sandy2

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     Sandy APP
+    sandy test
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
